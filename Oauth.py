7d6b08eca070778f9393a540720f8add1eb68474
Yannick Djomo <67871275+franckya@users.noreply.github.com>
2022-05-05 06:10:22 -0700

    Create Oauth.py

---
+++ Oauth.py
@@ -0,0 +1,34 @@
+import requests
+from pyquery import PyQuery as pq
+
+client_id = '...your_mattermost_client_id...'
+user = '...username...'
+pw = '...userpass...'
+
+gitlab = 'https://git.example'
+chat = 'https://chat.opennms.com'
+team = '/opennms-it'
+
+r = requests.get(
+    chat + team + '/login/github'
+)
+q = pq(r.content)
+csrf_token = q('#new_ldap_user input[name="authenticity_token"]')[0].value  # watch out, several tokens for ldap vs. normal login, inspect the page to find correct one
+
+r2 = requests.post(
+    gitlab + '/users/auth/ldapmain/callback',  # or whatever the browser callback for your auth-method was
+    cookies=r.cookies,
+    data={
+        'authenticity_token': csrf_token,
+        'username': user,
+        'password': pw,
+    }
+)
+
+# print [h.url for h in r2.history] + [r2.url]  # to check the redirects
+
+me = requests.get(
+    chat + '/api/v1/users/me',
+    cookies=r2.cookies,
+)
+print me.json()  # if everything went well you're now authorized
